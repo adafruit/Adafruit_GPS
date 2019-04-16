3e80b5b74edafc18e34917fafedc1610572e0732
Travis CI <travis@travis-ci.org>
2019-04-16 13:56:26 +0000

    Deploy code docs to GitHub Pages Travis build: 8
    Commit: c149283ea835e0cb3889ffde3c7cd5bf12a9ac89

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['waitforsentence',['waitForSentence',['../class_adafruit___g_p_s.html#a93a140ac59cff5a034eff003d25935dd',1,'Adafruit_GPS']]],
+  ['waitforsentence',['waitForSentence',['../class_adafruit___g_p_s.html#a7be8125ecdbe57a21c17fb8b1f621b96',1,'Adafruit_GPS']]],
   ['wakeup',['wakeup',['../class_adafruit___g_p_s.html#a032614ec7013f7e109a111d974c69d6e',1,'Adafruit_GPS']]]
 ];
