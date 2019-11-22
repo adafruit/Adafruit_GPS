d1c690e6f852d6d95d6c491bf331bc6ded1fec6a
Travis CI <travis@travis-ci.org>
2019-11-22 20:39:37 +0000

    Deploy code docs to GitHub Pages Travis build: 62
    Commit: 99540e84047a6d0c8ffb84d1b3b52e792491da81

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['adafruit_5fgps',['Adafruit_GPS',['../class_adafruit___g_p_s.html',1,'Adafruit_GPS'],['../class_adafruit___g_p_s.html#a3dd74265015def9c893170f38bab1b61',1,'Adafruit_GPS::Adafruit_GPS(HardwareSerial *ser)'],['../class_adafruit___g_p_s.html#a2a7f90b64349dca658dbc8e0c79c9938',1,'Adafruit_GPS::Adafruit_GPS(TwoWire *theWire)']]],
+  ['adafruit_5fgps',['Adafruit_GPS',['../class_adafruit___g_p_s.html',1,'Adafruit_GPS'],['../class_adafruit___g_p_s.html#a3dd74265015def9c893170f38bab1b61',1,'Adafruit_GPS::Adafruit_GPS(HardwareSerial *ser)'],['../class_adafruit___g_p_s.html#a2a7f90b64349dca658dbc8e0c79c9938',1,'Adafruit_GPS::Adafruit_GPS(TwoWire *theWire)'],['../class_adafruit___g_p_s.html#a8422d8a2c060c172c179ebefb03bb67a',1,'Adafruit_GPS::Adafruit_GPS(SPIClass *theSPI, int8_t cspin)']]],
   ['adafruit_5fgps_2ecpp',['Adafruit_GPS.cpp',['../_adafruit___g_p_s_8cpp.html',1,'']]],
   ['adafruit_5fgps_2eh',['Adafruit_GPS.h',['../_adafruit___g_p_s_8h.html',1,'']]],
   ['altitude',['altitude',['../class_adafruit___g_p_s.html#acfe7032b7e7d4c2b4faa424f46a95b94',1,'Adafruit_GPS']]],
--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['gps_5fdefault_5fi2c_5faddr',['GPS_DEFAULT_I2C_ADDR',['../_adafruit___g_p_s_8h.html#a09a011d0f9a94852f1cf93de299f6152',1,'Adafruit_GPS.h']]],
-  ['gps_5fmax_5fi2c_5ftransfer',['GPS_MAX_I2C_TRANSFER',['../_adafruit___g_p_s_8h.html#aa4f51a6693bb8e084e5b879528dee0bf',1,'Adafruit_GPS.h']]]
+  ['gps_5fmax_5fi2c_5ftransfer',['GPS_MAX_I2C_TRANSFER',['../_adafruit___g_p_s_8h.html#aa4f51a6693bb8e084e5b879528dee0bf',1,'Adafruit_GPS.h']]],
+  ['gps_5fmax_5fspi_5ftransfer',['GPS_MAX_SPI_TRANSFER',['../_adafruit___g_p_s_8h.html#a2a9a79e8518d2e3ca5791a112a1976a5',1,'Adafruit_GPS.h']]]
 ];
