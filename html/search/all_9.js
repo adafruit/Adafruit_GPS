e251b55986e42489cf02b50e90a0886f8086fcbb
Doxygen CI <travis@travis-ci.org>
2020-01-18 22:00:07 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -3,6 +3,7 @@ var searchData=
   ['newnmeareceived',['newNMEAreceived',['../class_adafruit___g_p_s.html#accb1fa960283447d2868c59951f0a9a1',1,'Adafruit_GPS']]],
   ['nmea_5fbad',['NMEA_BAD',['../_adafruit___g_p_s_8h.html#aea6700399d9215ce5ef0f450d1733dcaa503c4b54b94d0e729566af15b72f76f2',1,'Adafruit_GPS.h']]],
   ['nmea_5fcheck_5ft',['nmea_check_t',['../_adafruit___g_p_s_8h.html#aea6700399d9215ce5ef0f450d1733dca',1,'Adafruit_GPS.h']]],
+  ['nmea_5fextensions',['NMEA_EXTENSIONS',['../_adafruit___g_p_s_8h.html#aca54bdead693302e0ed647eaa65c56af',1,'Adafruit_GPS.h']]],
   ['nmea_5fhas_5fchecksum',['NMEA_HAS_CHECKSUM',['../_adafruit___g_p_s_8h.html#aea6700399d9215ce5ef0f450d1733dcaa9ec09ffff6d9cb41a22d2780523646ba',1,'Adafruit_GPS.h']]],
   ['nmea_5fhas_5fdollar',['NMEA_HAS_DOLLAR',['../_adafruit___g_p_s_8h.html#aea6700399d9215ce5ef0f450d1733dcaa8d5fd241c3361bef95f143797347c887',1,'Adafruit_GPS.h']]],
   ['nmea_5fhas_5fname',['NMEA_HAS_NAME',['../_adafruit___g_p_s_8h.html#aea6700399d9215ce5ef0f450d1733dcaa092e0bfd70702561109e85dc8c249810',1,'Adafruit_GPS.h']]],
--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___g_p_s.html#ab86462ef1409d2bd8ff7a8405c70ff34',1,'Adafruit_GPS']]]
+  ['begin',['begin',['../class_adafruit___g_p_s.html#ab86462ef1409d2bd8ff7a8405c70ff34',1,'Adafruit_GPS']]],
+  ['build',['build',['../class_adafruit___g_p_s.html#a75dfcfa9fd0ab68ddaa7e7b1ec796a16',1,'Adafruit_GPS']]]
 ];
