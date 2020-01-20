716315af9716c3e4f3595bdbaac2f7c3b9ea483d
Rick Sellens <sellensr@gmail.com>
2020-01-19 20:16:24 -0500

    single is not float
    that's what happens when you rush

--- src/NMEA_data.h
+++ src/NMEA_data.h
@@ -28,7 +28,7 @@
   hit, and closer than needed for navigation, so floats can be used to save a
   little storage.
  **************************************************************************/
-typedef single
+typedef float
     nmea_float_t; ///< the type of variables to use for floating point
 
 /**************************************************************************/
