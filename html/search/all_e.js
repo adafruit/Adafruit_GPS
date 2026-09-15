058431739468e41ce92294bde2bd10ad61281a37
Doxygen CI <ci-arduino@invalid>
2026-09-15 15:05:45 +0000

    Deploy docs to GitHub Pages from commit 6c9aba2fa8
    Commit: 6c9aba2fa8802cf1d52858df7fd15452b63cd957
    GitHub Actions run: 34985113297

--- html/search/all_e.js
+++ html/search/all_e.js
@@ -6,7 +6,7 @@ var searchData=
   ['secondssincedate',['secondsSinceDate',['../class_adafruit___g_p_s.html#a4b49f40ba946d1646d7937b25e8dda37',1,'Adafruit_GPS']]],
   ['secondssincefix',['secondsSinceFix',['../class_adafruit___g_p_s.html#aed7db1b8b55742a78f291687f1ebc9ca',1,'Adafruit_GPS']]],
   ['secondssincetime',['secondsSinceTime',['../class_adafruit___g_p_s.html#af01989c447efddb9fe1baa78e50373c5',1,'Adafruit_GPS']]],
-  ['sendcommand',['sendCommand',['../class_adafruit___g_p_s.html#abd7e2f17f11188dfc99fff55e3560dfb',1,'Adafruit_GPS']]],
+  ['sendcommand',['sendCommand',['../class_adafruit___g_p_s.html#abd7e2f17f11188dfc99fff55e3560dfb',1,'Adafruit_GPS::sendCommand(const char *)'],['../class_adafruit___g_p_s.html#aa2de930139bef6da9433272a2b9b30b5',1,'Adafruit_GPS::sendCommand(const __FlashStringHelper *)']]],
   ['showdatavalue',['showDataValue',['../class_adafruit___g_p_s.html#accf44cdc598df5c687e2f5fb32e9e752',1,'Adafruit_GPS']]],
   ['smoothed',['smoothed',['../structnmea__datavalue__t.html#a067a769e6b0ef402b3bf01b0c333e269',1,'nmea_datavalue_t']]],
   ['speed',['speed',['../class_adafruit___g_p_s.html#a449f4a33ecbe0fdf656f6663c74e6cce',1,'Adafruit_GPS']]],
