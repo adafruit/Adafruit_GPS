2cb1cf4b6cfcfc94ed57649f15ca1d4a6d670f2c
Doxygen CI <ci-arduino@invalid>
2026-09-16 00:30:58 +0000

    Deploy docs to GitHub Pages from commit 990dd27ae3
    Commit: 990dd27ae38003548f3fc008a268e16df1dc7b70
    GitHub Actions run: 35039703878

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -5,6 +5,7 @@ var searchData=
   ['lastnmea',['lastNMEA',['../class_adafruit___g_p_s.html#a6938dadca5d72ec6e89e86a5b01b9b67',1,'Adafruit_GPS']]],
   ['lastsentence',['lastSentence',['../class_adafruit___g_p_s.html#a35d9d4c069a8ac93cbcbc98cf1c86ba0',1,'Adafruit_GPS::lastSentence()'],['../class_adafruit___n_m_e_a.html#a144bf137d190ef58c95ed1f170d2f6eb',1,'Adafruit_NMEA::lastSentence()']]],
   ['lastsource',['lastSource',['../class_adafruit___g_p_s.html#a8c44f113dda3b54b41a7d05f516c9ec0',1,'Adafruit_GPS']]],
+  ['lasttext',['lastText',['../class_adafruit___n_m_e_a.html#a818739ffc3a6a2b9f93f0f72ef82e75e',1,'Adafruit_NMEA']]],
   ['lastupdate',['lastUpdate',['../structnmea__datavalue__t.html#ad19f5539f1b0524e3e8b2df9e3a07ae8',1,'nmea_datavalue_t']]],
   ['lat',['lat',['../class_adafruit___g_p_s.html#a2b04c8136e976cd8cd87aaa3526753d6',1,'Adafruit_GPS']]],
   ['latest',['latest',['../structnmea__datavalue__t.html#aa6e709073decb4b329756e910110e602',1,'nmea_datavalue_t']]],
--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['lastnmea',['lastNMEA',['../class_adafruit___g_p_s.html#a6938dadca5d72ec6e89e86a5b01b9b67',1,'Adafruit_GPS']]],
   ['lastsentence',['lastSentence',['../class_adafruit___n_m_e_a.html#a144bf137d190ef58c95ed1f170d2f6eb',1,'Adafruit_NMEA']]],
+  ['lasttext',['lastText',['../class_adafruit___n_m_e_a.html#a818739ffc3a6a2b9f93f0f72ef82e75e',1,'Adafruit_NMEA']]],
   ['locus_5freadstatus',['LOCUS_ReadStatus',['../class_adafruit___g_p_s.html#adb6139e84c64156b5654dfce2a2e6e3f',1,'Adafruit_GPS']]],
   ['locus_5fstartlogger',['LOCUS_StartLogger',['../class_adafruit___g_p_s.html#a325d15c43c09fa1bedcdadb39706e72b',1,'Adafruit_GPS']]],
   ['locus_5fstoplogger',['LOCUS_StopLogger',['../class_adafruit___g_p_s.html#a48eaf7f5b46e3682b058a7f8f44fa385',1,'Adafruit_GPS']]]
