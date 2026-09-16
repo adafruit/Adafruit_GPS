f11b7bae88eef7d314790c3d732a3c5b9ca8c279
Doxygen CI <ci-arduino@invalid>
2026-09-16 17:02:23 +0000

    Deploy docs to GitHub Pages from commit 6a569968e1
    Commit: 6a569968e1b4c216aadff59d0a5b07b4e2416cf7
    GitHub Actions run: 35124936634

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['lastnmea',['lastNMEA',['../class_adafruit___g_p_s.html#a6938dadca5d72ec6e89e86a5b01b9b67',1,'Adafruit_GPS']]],
-  ['lastposition',['lastPosition',['../class_adafruit___g_n_s_s.html#aaf32a92103e32ef61379414c1b152c2a',1,'Adafruit_GNSS']]],
+  ['lastposition',['lastPosition',['../class_adafruit___g_n_s_s.html#aaf32a92103e32ef61379414c1b152c2a',1,'Adafruit_GNSS::lastPosition()'],['../class_adafruit___g_p_s.html#add173e9cb6547f3b265dd15491f893aa',1,'Adafruit_GPS::lastPosition()']]],
   ['lastsentence',['lastSentence',['../class_adafruit___n_m_e_a.html#a144bf137d190ef58c95ed1f170d2f6eb',1,'Adafruit_NMEA']]],
   ['lasttext',['lastText',['../class_adafruit___n_m_e_a.html#a818739ffc3a6a2b9f93f0f72ef82e75e',1,'Adafruit_NMEA']]],
   ['locus_5freadstatus',['LOCUS_ReadStatus',['../class_adafruit___g_p_s.html#adb6139e84c64156b5654dfce2a2e6e3f',1,'Adafruit_GPS']]],
